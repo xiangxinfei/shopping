83fa2ddd4b2ae19674d157edbcf57bbcaa6b2aba
xiangxinfei <1585115154@qq.com>
2019-12-08 16:19:15 +0800

    引入字体图标

---
+++ components/iconfont/index.js
@@ -0,0 +1,18 @@
+Component({
+  data: {},
+  properties: {
+    type: {
+      type: String,
+      value: 'danju'
+    },
+    size: {
+      type: String,
+      value: 28
+    },
+    color: {
+      type: String,
+      value: '#999'
+    },
+  },
+  methods: {}
+})
